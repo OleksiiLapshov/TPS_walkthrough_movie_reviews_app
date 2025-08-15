17f25cea84ef6fe46b661c5f5231e2711f090bba
Oleksii Lapshov <izraphail@gmail.com>
2025-08-15 17:49:19 +0300

    added a few custom helpers; moved "business logic" to model

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,13 @@
 module MoviesHelper
+  def shortener(movie)
+    truncate(movie.description, length: 35, separator: ' ')
+  end
+
+  def total_gross(movie)
+    movie.flop? ? "Flop" : number_to_currency(movie.total_gross, precision: 0)
+  end
+
+  def formated_date(movie)
+    movie.released_on.strftime("%B %Y")
+  end
 end
