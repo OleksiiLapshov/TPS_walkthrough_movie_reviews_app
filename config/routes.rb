cd4c17e44d059ae781e75cbd799dc5d7241027c9
Oleksii Lapshov <izraphail@gmail.com>
2025-08-30 19:54:32 +0300

    created sign in controller and a view

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
 
   root "movies#index"
 
+  resource :session, only: [ :new, :create, :destroy ]
+
   resources :movies do
     resources :reviews
   end
 
   resources :users
   get "signup" => "users#new"
+
+  resource :session, only: [ :new, :create, :destroy ]
+  get "signin" => "sessions#new"
 end
