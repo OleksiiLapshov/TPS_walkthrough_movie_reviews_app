961d36044eccd0eba6f561c9a645cfdfd540d141
Oleksii Lapshov <izraphail@gmail.com>
2025-09-06 11:07:59 +0300

    added simple session creation/auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Welcome back, #{user.name}"
+    else
+      flash.now[:alert] = "Wrong username or password!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
