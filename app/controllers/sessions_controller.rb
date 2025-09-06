859d78346a1d0e834b6afc9a27500a212022fd2b
Oleksii Lapshov <izraphail@gmail.com>
2025-09-06 13:59:41 +0300

    fixed bug with login (incorrect param name lead to sign in to an account with nil username); added signin logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email_or_username]) ||
-           User.find_by(username: params[:email_or_username])
+    user = User.find_by(email: params[:email_or_nickname]) ||
+           User.find_by(username: params[:email_or_nickname])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_url(@user), notice: "User has been created!"
     else
       render :new, status: :unprocessable_entity
