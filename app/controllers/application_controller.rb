0cb548a17d6394921126bb98842ba8f0e6149ea3
Oleksii Lapshov <izraphail@gmail.com>
2025-09-06 19:48:01 +0300

    added sign in check for some actions for the user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+private
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_signin
+    unless currect_user
+      redirect_to new_session_url, alert: "Sign in first!"
+    end
+  end
+
+  helper_method :current_user
 end
