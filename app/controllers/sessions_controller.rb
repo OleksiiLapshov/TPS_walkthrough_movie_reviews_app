cbe988a8025b09c2ebf4a9ab4eaec6369eeb682b
Oleksii Lapshov <izraphail@gmail.com>
2025-09-06 11:39:33 +0300

    minor change to accept not only email but nicknames too

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:email_or_username]) ||
+           User.find_by(username: params[:email_or_username])
+
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user, notice: "Welcome back, #{user.name}"
     else
-      flash.now[:alert] = "Wrong username or password!"
+      flash.now[:alert] = "Wrong email/nickname or password!"
       render :new, status: :unprocessable_entity
     end
   end
