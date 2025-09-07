c10eb3804f78f44f1b6af9c135f880da4ff19319
Oleksii Lapshov <izraphail@gmail.com>
2025-09-07 15:18:46 +0300

    added migration to support admin/non-admin users; added some logic to limit certain actions only for admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,19 @@ private
     end
   end
 
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Access denied!"
+    end
+  end
+
   def current_user?(user)
     current_user == user
   end
   helper_method :current_user?
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+  helper_method :current_user_admin?
 end
