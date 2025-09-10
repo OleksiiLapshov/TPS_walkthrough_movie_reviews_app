7f44bba3a7d2cc10777d8ba30d7f937748b8856f
Oleksii Lapshov <izraphail@gmail.com>
2025-09-10 20:16:22 +0300

    added changes to MVC to handle fav feature (now user profiles and movies page are showing favs)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_secure_password
 
+  has_many :favorite_movies, through: :favorites, source: :movie
+
   validates :name, presence: true
   validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 10, allow_blank: true }
