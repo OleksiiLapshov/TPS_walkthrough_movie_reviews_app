e3b00187337715e8c8d9671f9b95e7c83361f12e
Oleksii Lapshov <izraphail@gmail.com>
2025-09-12 22:35:40 +0300

    added filters for movies based on scopes and nav-links with a highlight

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :genres
 
   root "movies#index"
+  get "movies/filter/:filter" => "movies#index", as: :filtered_movies
 
   resource :session, only: [ :new, :create, :destroy ]
   get "signin" => "sessions#new"
